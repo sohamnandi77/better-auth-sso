ee7088d47ce901328afbb840d8ccfef68a08ec9c
Soham Nandi <sohamnandi77@gmail.com>
2024-11-13 00:10:02 +0530

    Revert "feat(better-auth-idp): remove redundant CORS header for Access-Control-Allow-Origin"
    This reverts commit 707b8b1ffedb272311f21760ca3953b6e6df0336.

--- apps/better-auth-idp/next.config.ts
+++ apps/better-auth-idp/next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         source: "/api/:path*",
         headers: [
           {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
             key: "Access-Control-Allow-Credentials",
             value: "true",
           },
